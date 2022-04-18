03c4295057cd256aaac4394caf4024a47f147a59
Melika Heidari <53211989+Melikad@users.noreply.github.com>
2022-04-18 23:38:32 +0430

    Update test(1).c

--- test(1).c
+++ test(1).c
@@ -5,14 +5,14 @@ int main(int argc, char *argv[])
         int child2 = fork();
         if(child2 == 0){
             write(1,"the second child\n", strlen("the second child\n"));
-            for(int i = 0 ; i < 2000000000 ; i++){}
+            for(int i = 0 ; i < 1000000000 ; i++){}
             write(1,"Second child exits\n", strlen("Second child exits\n"));
             exit();
             
         }
         write(1,"the first child waiting for Second child\n", strlen("the first child waiting for Second child\n"));
         waitForProcess(child2);
-        for(int i = 0 ; i < 2000000000 ; i++){}
+        for(int i = 0 ; i < 1000000000 ; i++){}
         write(1,"First child exits\n", strlen("First child exits\n"));
         exit();
     }
